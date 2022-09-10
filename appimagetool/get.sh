551e4765487988343cb7c601fd1119654d76eaa4
numamas <you@example.com>
2022-09-10 18:50:17 +0900

    init

---
+++ appimagetool/get.sh
@@ -0,0 +1,7 @@
+# appimagetool
+curl -LO https://github.com/AppImage/AppImageKit/releases/download/13/appimagetool-x86_64.AppImage
+mv appimagetool-x86_64.AppImage appimagetool
+chmod a+x appimagetool
+
+# functions.sh
+curl -LO https://github.com/probonopd/AppImages/raw/master/functions.sh
---
+++ config.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+VERSION='28.1'
+GNUTLS_VERSION='28'
+
+ROOT="$(cd "$(dirname "$0")"; pwd)"
+INSTALL_DIR="/opt/emacs"
+
